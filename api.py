d43f4c4f0645e452578eb8921aa8ef278c43f11a
tpk256 <tpk256@mail.ru>
2022-12-21 13:51:17 +0500

    GLobal change

--- api.py
+++ api.py
@@ -1,5 +1,7 @@
 import requests
 
+import json
+
 from matplotlib import dates
 
 import matplotlib.pyplot as plt
@@ -8,172 +10,193 @@ import numpy as np
 
 import datetime
 
-id_ = 1
-# Todo переделать почеловечески,а то как-то костыльно
+import logging
 
 
-def increment_id():
-    global id_
-    id_ += 1
+class Zabbix:
+    def __init__(self, user, password, url, host):
+        self.user = user
+        self.password = password
+        self.url = url
+        self.token = None
+        self.id_ = 1
+        self.state_auth = False
+        self.host = host
 
-
-def get_token(user, password, url):
-    payload = {
-        "jsonrpc": "2.0",
-        "method": "user.login",
-        "params": {
-            "user": user,
-            "password": password
-        },
-        "id": id_,
-        "auth": None}
-    answer = requests.post(url=url, json=payload).json()
-    increment_id()
-    if 'error' not in answer:
-        return answer['result']
-    print(answer)
-    exit(-1)
-
-
-def logout(token, url):
-
-    payload = {
+    def logout(self):
+        if not self.state_auth:
+            return
+        payload = {
             "jsonrpc": "2.0",
             "method": "user.logout",
             "params": [],
-            "id": id_,
-            "auth": token
+            "id": self.id_,
+            "auth": self.token
         }
-    answer = requests.post(url=url, json=payload).json()
-    increment_id()
-    if 'error' not in answer:
-        return answer['result']
-    print(answer)
+        answer = requests.post(url=self.url, json=payload)
 
+        try:
+            data = answer.json()
+            if "result" not in data:
+                exit(1)
 
-def get_history(host, token, url):
-    # add itemsids
-    payload = {
+        except json.JSONDecodeError:
+            exit(1)
+
+        self.id_ += 1
+        self.state_auth = False
+
+    def get_token(self):
+        if self.state_auth:
+            return
+
+        payload = {
+            "jsonrpc": "2.0",
+            "method": "user.login",
+            "params": {
+                "user": self.user,
+                "password": self.password
+            },
+            "id": self.id_,
+            "auth": None}
+        answer = requests.post(url=self.url, json=payload)
+        try:
+            data = answer.json()
+            if "result" not in data:
+                logging.error(data)
+                exit(1)
+        except json.JSONDecodeError:
+            exit(1)
+        self.id_ += 1
+        self.token = data['result']
+        self.state_auth = True
+
+    def __enter__(self):
+        self.get_token()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.logout()
+        return
+
+    def get_triggers_problem(self, ):
+        payload = {
+            "jsonrpc": "2.0",
+            "method": "trigger.get",
+            "params": {
+                "output": ["triggerid"],  # ID triggers that with problem
+                "host": self.host,
+                "filter": {
+                    "value": 1  # HAS PROBLEM
+                }
+            },
+            "auth": self.token,
+            "id": self.id_
+        }
+
+        answer = requests.post(url=self.url, json=payload)
+        self.id_ += 1
+        try:
+            data = answer.json()
+            if 'error' not in answer:
+                return [item['triggerid'] for item in data["result"]]
+        except json.JSONDecodeError:
+            ...
+
+        return []
+
+    def get_items_for_trigger(self, triggers, name=False):
+        payload = {
+            "jsonrpc": "2.0",
+            "method": "item.get",
+            "params": {
+                "triggerids": triggers,
+                "output": ["itemid", "name"] if name else "itemid",
+            },
+            "auth": self.token,
+            "id": self.id_
+        }
+
+        answer = requests.post(url=self.url, json=payload)
+        self.id_ += 1
+        try:
+            data = answer.json()
+            if 'error' not in answer:
+                if not name:
+                    return [item['itemid'] for item in data["result"]]
+                return [(item['itemid'], item['name']) for item in data["result"]]
+        except json.JSONDecodeError:
+            ...
+        return []
+
+#
+#
+    def get_history_item(self, item_id, type_="3", limit=30):
+        payload = {
             "jsonrpc": "2.0",
             "method": "history.get",
             "params": {
                 "output": "extend",
-                "history": 0,
-                "limit": 10,
-                "search": {
-                    "host": host,
-                    "Key": "agent.ping"
-                }
+                "history": type_,
+                "itemids": item_id,
+                "sortfield": "clock",
+                "sortorder": "DESC",
+                "limit": limit
             },
-            "auth": token,
-            "id": id_
+            "auth": self.token,
+            "id": self.id_
         }
-    increment_id()
-    answer = requests.post(url=url, json=payload).json()
 
-    if 'error' not in answer:
-        return answer
-    print(answer)
+        self.id_ += 1
+        answer = requests.post(url=self.url, json=payload)
 
+        try:
+            data = answer.json()
+            if 'error' not in answer:
+                return sorted(data['result'], key=lambda dct: int(dct['clock']))
+        except json.JSONDecodeError:
+            ...
+        return []
 
-def get_triggers_problem(host, token, url, desc=False):
-    payload = {
-        "jsonrpc": "2.0",
-        "method": "trigger.get",
-        "params": {
-            "output": ["triggerid", "description"] if desc else "triggerid",  # ID triggers that with problem
-            "host": host,
-            "filter": {
-                "value": 1  # HAS PROBLEM
-            }
-        },
-        "auth": token,
-        "id": id_
-    }
-    increment_id()
-    answer = requests.post(url=url, json=payload).json()
+    def get_type_for_item(self, item):
+        payload = {
+            "jsonrpc": "2.0",
+            "method": "item.get",
+            "params": {
+                "output": ["value_type"],
+                "itemids": item,
+            },
+            "auth": self.token,
+            "id": self.id_
+        }
+        self.id_ += 1
+        answer = requests.post(url=self.url, json=payload)
 
-    if 'error' not in answer:
-        return answer["result"]
-    print(answer)
+        try:
+            data = answer.json()
+            if 'error' not in answer:
+                return data['result'][-1]['value_type']
+        except json.JSONDecodeError:
+            ...
+        return -100
 
+    @staticmethod
+    def convert_timestamp_to_datetime(timestamp_list):
+        return [datetime.datetime.fromtimestamp(item, tz=None) for item in timestamp_list]
 
-def get_items_for_trigger(trigger, token, url, name=False):
-    payload = {
-        "jsonrpc": "2.0",
-        "method": "item.get",
-        "params": {
-            "triggerids": trigger,
-            "output": ["itemid", "name"] if name else "itemid",
-        },
-        "auth": token,
-        "id": id_
-    }
-    increment_id()
-    answer = requests.post(url=url, json=payload).json()
-    if 'error' not in answer:
-        return answer["result"]
-    print(answer)
+    @staticmethod
+    def create_graph(x, y, name):
+        fmt = dates.DateFormatter('%d-%H:%M')
+        fig, ax = plt.subplots()
 
+        plt.title(f"График изменения {name}")
+        plt.xlabel("Datetime")
+        plt.ylabel("Values")
+        ax.grid()
 
-def get_history_item(item_id, token, url, type_=3, limit=100):
-    payload = {
-        "jsonrpc": "2.0",
-        "method": "history.get",
-        "params": {
-            "output": "extend",
-            "history": type_,
-            "itemids": item_id,
-            "sortfield": "clock",
-            "sortorder": "DESC",
-            "limit": limit
-        },
-        "auth": token,
-        "id": id_
-    }
+        ax.plot(x, y, "-o")
+        ax.xaxis.set_major_formatter(fmt)
+        fig.autofmt_xdate()
 
-    increment_id()
-    answer = requests.post(url=url, json=payload).json()
-
-    if 'error' not in answer:
-        return answer['result']
-    print(answer)
-
-
-def get_type_for_item(token, url,  item):
-    payload = {
-        "jsonrpc": "2.0",
-        "method": "item.get",
-        "params": {
-            "output": ["value_type"],
-            "itemids": item,
-        },
-        "auth": token,
-        "id": id_
-    }
-    increment_id()
-    answer = requests.post(url=url, json=payload).json()
-
-    if 'error' not in answer:
-        return answer['result']
-    print(answer)
-
-
-def convert_timestamp_to_datetime(timestamp_list):
-    return [datetime.datetime.fromtimestamp(item, tz=None) for item in timestamp_list]
-
-
-def create_graph(x, y, name):
-    fmt = dates.DateFormatter('%d-%H:%M')
-    fig, ax = plt.subplots()
-
-    plt.title(f"График изменения {name}")
-    plt.xlabel("Datetime")
-    plt.ylabel("Values")
-    ax.grid()
-
-    ax.plot(x, y, "-o")
-    ax.xaxis.set_major_formatter(fmt)
-    fig.autofmt_xdate()
-
-    fig.savefig(f"{name}.png")
+        fig.savefig(f"{name}.png")
+        return f"{name}.png"
--- main.py
+++ main.py
@@ -1,37 +1,106 @@
+import os
 
-from api import *
+from api import Zabbix
+
+from expressAPi import *
+
+import sys
 
 
-# спарсить
-# имя хоста поулчить
-host = "Zabbix server"
-zabbix_url_api = "http://127.0.0.1:80/api_jsonrpc.php"
-name = "ping"
-item_id = 0
+if len(sys.argv) != 13:
+    exit(-1)
 
-token = get_token(user="Admin", password="zabbix", url=zabbix_url_api)
-print(token)
-trgs = [trg["triggerid"] for trg in get_triggers_problem(host, token, zabbix_url_api)]
-print(trgs, "trgs")
+event_source, \
+    host, \
+    user, \
+    password, \
+    url, \
+    event_update_status, \
+    event_value, \
+    express_message, \
+    express_send_to, \
+    express_tags,\
+    express_token, \
+    express_url = sys.argv[1:]
 
-items = [get_items_for_trigger(trg, token, zabbix_url_api, True)[0]["itemid"] for trg in trgs]
-print(items, "items")
 
-items_types = [(item, get_type_for_item(token, zabbix_url_api, item)[0]['value_type']) for item in items ]
-print(items_types)
-# items = ['28615']
+# Ignore event_source because we use only triggers
+# Ignore event_update_status
+# What's mean express_tags?
 
-hs = [get_history_item(item, token, zabbix_url_api, type_, ) for item, type_ in items_types]
 
-for_gr = sorted(hs[0], key=lambda dct: int(dct['clock']))
+def send_problem(name):
+    try:
+        with open(name, mode="rb") as f:
+            data = f.read()
 
-x = []
-y = []
-for item in for_gr:
-    x += [int(item['clock'])]
-    y += [int(item['value'])]
+        url = express_url + "/api/v4/botx/notifications/direct"
+        headers = {"Authorization": f"Bearer {express_token}",
+                   "Content-Type": "application/json"}
 
-x = convert_timestamp_to_datetime(x)
-create_graph(x, y, "number_process")
+        payload = {"group_chat_id": express_send_to,
+                   "notification": {
+                       "status": "ok",
+                       "body": express_message,
+                   }}
+        req = requests.post(url, json=payload, headers=headers)
 
-# logout(token, url=zabbix_url_api)
+        multi_part = {"content": (f"{name}", f"{data}", "image/png"),
+                      "group_chat_id": (None, express_send_to),
+                      "meta": (None,
+                               json.dumps({
+                                   "duration": None,
+                                   "caption": f"{name}"
+                               }),
+                               "application/json")
+
+                      }
+        url = express_url + "/api/v3/botx/files/upload"
+        req = requests.post(url, files=multi_part, headers=headers)
+
+        if not req.ok:
+            exit(-1)
+        return req.json()
+    except requests.Timeout:
+        exit(-1)
+
+
+# event_value = 1
+# user = "Admin"
+# password = "zabbix"
+# url = "http://127.0.0.1:80/api_jsonrpc.php"
+# host = "Zabbix server"
+#
+
+
+if __name__ == "__main__":
+    if event_value == 1:  # It's a problem
+        zb = Zabbix(user, password, url, host)
+        with zb:
+            trgs = zb.get_triggers_problem()
+            if not trgs:
+                exit(0)
+
+            items = {}
+            for value in zb.get_items_for_trigger(trgs, name=True):
+                if len(value) == 1:
+                    items[value[0]] = []
+                else:
+                    items[value[0]] = [value[1]]
+            for key in items:
+                type_ = zb.get_type_for_item(key)
+                items[key] += [type_]
+            for key in items:
+                history = zb.get_history_item(key, items[key][-1])
+
+                values = sorted(history, key=lambda clk: clk['clock'])
+                items[key] += [zb.convert_timestamp_to_datetime(int(item['clock']) for item in values)]
+                items[key] += [[item['value'] for item in values]]
+
+        for key in items:
+            filename = "./" + zb.create_graph(items[key][-2], items[key][-1], items[key][0])
+            send_problem(filename)
+
+            if os.path.exists(filename):
+                os.remove(filename)
+
